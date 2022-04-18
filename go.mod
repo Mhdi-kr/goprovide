c7a5e6acfa7531434e2550d8fa1453c43af6d16f
Mahdi  Karimi <mhdi_kr@outlook.com>
2022-04-18 08:29:43 +0430

    feat

--- go.mod
+++ go.mod
@@ -10,6 +10,9 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.34.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/go-resty/resty/v2"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 type context interface {
@@ -72,31 +78,107 @@ func newContainer() {
 
 }
 
-func main() {
-	actions := ProviderActions{
-		authenticate: func(client *resty.Client) bool {
-			resp, err := client.R().Get("https://www.example.com")
-			fmt.Println(resp, err)
-			return true
-		},
-		available: func() bool {
-			return true
-		},
-		prebook: func() bool {
-			return true
-		},
-		book: func() bool {
-			return true
-		},
-		confirm: func() bool {
-			return true
-		},
-		cancel: func() bool {
-			return true
-		},
-		refund: func() bool {
-			return true
-		},
+func NewZapLogger() *zap.Logger {
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	logger, _ := config.Build()
+	defer logger.Sync()
+	return logger
+}
+
+func compactJson(jsonBytes []byte) string {
+	buffer := new(bytes.Buffer)
+	if err := json.Compact(buffer, jsonBytes); err != nil {
+		fmt.Println(err)
 	}
-	newProvider("ADOTEL", actions)
+	return buffer.String()
+}
+
+func NewRestyClient() *resty.Client {
+	logger := NewZapLogger()
+	client := resty.New()
+	client.OnBeforeRequest(func(c *resty.Client, req *resty.Request) error {
+		logger.Info("HTTP request",
+			zap.String("endpoint", req.URL),
+			zap.Int("attempt", 3),
+			zap.Duration("backoff", time.Second),
+		)
+		return nil
+	})
+
+	client.OnAfterResponse(func(c *resty.Client, resp *resty.Response) error {
+		switch resp.StatusCode() {
+		case http.StatusOK:
+			logger.Info("HTTP response",
+				zap.String("status", resp.Status()),
+				zap.String("body", compactJson(resp.Body())),
+				zap.Int("attempt", 3),
+				zap.Duration("backoff", time.Second),
+			)
+		case http.StatusNotFound:
+			logger.Info("HTTP response",
+				zap.String("status", resp.Status()),
+				zap.String("body", compactJson(resp.Body())),
+				zap.Int("attempt", 3),
+				zap.Duration("backoff", time.Second),
+			)
+		}
+
+		return nil
+	})
+
+	client.OnError(func(req *resty.Request, err error) {
+		fmt.Println("asd")
+		if _, ok := err.(*resty.ResponseError); ok {
+			// fmt.Print(v.Error())
+			// v.Response contains the last response from the server
+			// v.Err contains the original error
+		}
+		// Log the error, increment a metric, etc...
+	})
+	return client
+}
+
+func main() {
+	// url := "google.com"
+	httpClient := NewRestyClient()
+	httpClient.R().Get("https://jsonplaceholder.typicode.com/todoss/1")
+	// logger.Error("failed to fetch URL",
+	// 	// Structured context as strongly typed Field values.
+	// 	zap.String("url", url),
+	// 	zap.Int("attempt", 3),
+	// 	zap.Duration("backoff", time.Second),
+	// )
+	// logger.Fatal("failed to fetch URL",
+	// 	// Structured context as strongly typed Field values.
+	// 	zap.String("url", url),
+	// 	zap.Int("attempt", 3),
+	// 	zap.Duration("backoff", time.Second),
+	// )
+	// actions := ProviderActions{
+	// 	authenticate: func(client *resty.Client) bool {
+	// 		resp, err := client.R().Get("https://www.example.com")
+	// 		fmt.Println(resp, err)
+	// 		return true
+	// 	},
+	// 	available: func() bool {
+	// 		return true
+	// 	},
+	// 	prebook: func() bool {
+	// 		return true
+	// 	},
+	// 	book: func() bool {
+	// 		return true
+	// 	},
+	// 	confirm: func() bool {
+	// 		return true
+	// 	},
+	// 	cancel: func() bool {
+	// 		return true
+	// 	},
+	// 	refund: func() bool {
+	// 		return true
+	// 	},
+	// }
+	// newProvider("ADOTEL", actions)
 }
